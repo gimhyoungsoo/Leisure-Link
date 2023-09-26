62fbd4ac3c98afd5ced5a3722e6d5605a89e9b56
toa <gimhyoungsoo@gmail.com>
2023-09-26 19:28:48 +0900

    🎉 프로젝트 리팩토링 시작

--- server/grilledshrimp/src/main/java/com/mainproject/grilledshrimp/domain/postTag/dto/PostTagPostDto.java
+++
@@ -1,6 +0,0 @@
-package com.mainproject.grilledshrimp.domain.postTag.dto;
-
-// 게시글 태그 생성
-public class PostTagPostDto {
-
-}
--- server/grilledshrimp/src/main/java/com/mainproject/grilledshrimp/domain/tag/dto/TagPostDto.java
+++
@@ -1,6 +0,0 @@
-package com.mainproject.grilledshrimp.domain.tag.dto;
-
-// 태그 생성
-public class TagPostDto {
-    private String tag_name;
-}
