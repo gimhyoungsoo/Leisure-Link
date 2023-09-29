0c0a32c21ed8335b4eb1567a8e2bf1da52d52be3
toa <gimhyoungsoo@gmail.com>
2023-09-29 17:22:38 +0900

    ✨비로그인 유저 추천 클릭시, 로그인 안내 추가

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -4,26 +4,23 @@ import React from "react";
 import "../pages/App.module.css";
 import SignupPage from "./SignupPage";
 import LoginPage from "./LoginPage";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Modal from "../components/Common/Modal";
-import { createPortal } from 'react-dom';
+import { createPortal } from "react-dom";
 function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/LoginPage" element={<LoginPage />} />
-          <Route path="/SignUpPage" element={<SignupPage />} />
-          <Route path="/MyPage" element={<MyPage />} />
-        </Routes>
-      </BrowserRouter>
-      {createPortal(<Modal/>, document.body)}
-    </>
-  );
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<MainPage />} />
+                    <Route path="/LoginPage" element={<LoginPage />} />
+                    <Route path="/SignUpPage" element={<SignupPage />} />
+                    <Route path="/MyPage" element={<MyPage />} />
+                </Routes>
+                {createPortal(<Modal />, document.body)}
+            </BrowserRouter>
+        </>
+    );
 }
 
 export default App;
-
-
-
